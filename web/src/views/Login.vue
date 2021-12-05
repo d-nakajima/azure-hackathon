7dcf319ad51d95f2ddcbaaf66ad5b20395124fe1
d-nakajima <nakajima.daishi0329@gmail.com>
2021-12-05 13:41:38 +0900

    improve login screen design

--- web/src/views/Login.vue
+++ web/src/views/Login.vue
@@ -1,19 +1,19 @@
 <template>
-  <div>
-    <template v-if="currentUserId == null">
-      <p>ログイン</p>
-      <input v-model="name" placeholder="input user name" />
-      <button @click="login(name)">ログイン</button>
-    </template>
-    <template v-else>
-      <p>ログイン中</p>
-      <p>currentUserId: {{ currentUserId }}</p>
-    </template>
+  <div class="container">
+    <div class="login">
+      <img class="login-logo" src="@/assets/images/logo.png" />
+      <div class="login-form">
+        <input class="login-form-textBox" v-model="name" placeholder="input user name (>= 5 chars)" />
+        <span class="login-form-button" :disabled="!canSubmit" :class="{ active: canSubmit }" @click="login(name)">
+          {{ submitting ? '送信中...' : 'ログイン' }}
+        </span>
+      </div>
+    </div>
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent, inject } from 'vue'
+import { defineComponent, inject, computed } from 'vue'
 import { ref } from '@vue/reactivity'
 import axios from 'axios'
 import { useCurrentUserKey, CurrentUserStore, User } from '@/composables/useCurrentUser'
@@ -21,7 +21,6 @@ import { useRouter } from 'vue-router'
 
 export default defineComponent({
   setup () {
-    const name = ref(null)
     const router = useRouter()
     const currentUserStore = inject(useCurrentUserKey) as CurrentUserStore
 
@@ -34,9 +33,15 @@ export default defineComponent({
       }
     }
 
+    const name = ref('')
+    const canSubmit = computed(() => name.value.length >= 5)
+    const submitting = ref(false)
     const login = (name: any) => {
+      if (!canSubmit.value) return
+      submitting.value = true
       axios.post(`${process.env.VUE_APP_API_ROOT}/auth`, { name }).then(res => {
         currentUserStore.setCurrentUser(res.data.user as User)
+        submitting.value = false
         redirectByStatus()
       })
     }
@@ -45,9 +50,55 @@ export default defineComponent({
 
     return {
       name,
+      canSubmit,
+      submitting,
       login,
       currentUserId: currentUserStore.currentUser.id
     }
   }
 })
 </script>
+
+<style lang="scss" scoped>
+.container {
+  background: $logo-background;
+  height: 100%;
+}
+.login {
+  padding: 50px;
+  &-title {
+    margin: 20px;
+    font-size: 24px;
+  }
+  &-form {
+    margin: 20px;
+    text-align: center;
+    &-textBox {
+      display: block;
+      height: 32px;
+      width: 100%;
+      max-width: 480px;
+      margin: auto;
+    }
+    &-button {
+      margin-top: 50px;
+      padding: 20px 40px;
+      display: inline-block;
+      border-radius: 10px;
+      background: $disable;
+      color: $gray;
+      &.active {
+        color: $black;
+        background: $button-background-active;
+        box-shadow: 1px 1px 2px $black;
+        pointer-events: inherit;
+      }
+    }
+  }
+  &-logo {
+    margin-top: 100px;
+    width: 80%;
+    max-width: 300px;
+  }
+}
+</style>
